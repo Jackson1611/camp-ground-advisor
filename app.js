4c65faf3b0742bd72ec5db7b439fb2341ca70031
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-03-03 11:12:33 +0200

    fixing mongo connection

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb://localhost:27012/camp", {
+mongoose.connect("mongodb://127.0.0.1:27017/test", {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
