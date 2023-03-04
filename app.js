f12ef047c76d4c01dee838e299e8d3d1b2b9fef3
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-03-04 12:54:18 +0200

    add basic styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejsmate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const mongoose = require("mongoose");
@@ -20,6 +21,7 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+app.engine("ejs", ejsmate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
