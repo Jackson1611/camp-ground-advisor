39ce521a943ff5fb54157f5e3f40ab14e5168d03
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-03-04 10:55:13 +0200

    CRUD completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const mongoose = require("mongoose");
 
@@ -22,9 +23,51 @@ db.once("open", () => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/campground", async (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+//show all camps
+app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render("campgorunds/index");
+  res.render("campgrounds/index", { campgrounds });
+});
+
+//add new camp
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+//save camp
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//edit camp
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+//show the camp choosen
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
 });
 
 app.get("/", (req, res) => {
