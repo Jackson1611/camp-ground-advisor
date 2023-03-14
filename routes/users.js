4f08f7003d316c07d1578123abc212ba5214f555
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-03-14 10:55:27 +0200

    fixed logout function

--- routes/users.js
+++ routes/users.js
@@ -46,9 +46,11 @@ router.post(
 );
 
 router.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success", "Goodbye!");
-  res.redirect("/campgrounds");
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash("success", "Goodbye!");
+    res.redirect("/campgrounds");
+  });
 });
 
 module.exports = router;
